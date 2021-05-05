5e717b8476b01f8143b328b3bd442eebd882ace9
ABIED Imad <Imad.Abied@bordeaux-inp.fr>
2021-05-05 17:25:27 +0200

    adapting the line length to nbr of choice

--- hist.py
+++ hist.py
@@ -15,11 +15,18 @@ class Hist(Scene):
     }
     
     def construct(self):
-        #params
-        line_height_from_bottom = 1
+        #your annimation param
+        nbr_choice = 2
 
+        #inter params
+
+        line_height_from_bottom = 1
+        segment_width = 1
+        segment_d_height = 0.2
+        
         #objects
-        line = Line(7.1*LEFT+(4-line_height_from_bottom)*DOWN,7.1*RIGHT+(4-line_height_from_bottom)*DOWN)
+        line_edge_x = segment_width*nbr_choice/2
+        line = Line(line_edge_x*LEFT+(4-line_height_from_bottom)*DOWN,line_edge_x*RIGHT+(4-line_height_from_bottom)*DOWN)
 
         #annimation
         self.play(ShowCreation(line))
